0d9d786f710fc96eb168a42e005ae75f3b230b3f
carlottasampels <privat@carlottasampels.de>
2025-02-28 12:25:25 +0100

    Add paths

---
+++ credentials/paths.R
@@ -0,0 +1,9 @@
+ilovedata <- "C:/Users/carlo/King's College London/MT-TNG BioResource EDIT - ilovedata - ilovedata"
+
+ilovedata_scripts <- "C:/Users/carlo/Documents/work/ilovedata/scripts/"
+
+filepath_glad_cleaned <- "C:/Users/carlo/King's College London/MT-TNG BioResource EDIT - ilovedata - ilovedata/data/latest_freeze/glad"
+
+filepath_data_raw <- "C:/Users/carlo/King's College London/MT-TNG BioResource EDIT - ilovedata - ilovedata/LGBTQ/data_raw"
+
+filepath_cleaned_data <- "C:/Users/carlo/King's College London/MT-TNG BioResource EDIT - ilovedata - ilovedata/LGBTQ/data/"
