49a0a51d5d582b878eb63861a363e690084f8d6f
carlottasampels <privat@carlottasampels.de>
2026-03-31 22:45:31 +0200

    updated

--- credentials/paths.R
+++ credentials/paths.R
@@ -8,6 +8,10 @@ filepath_data_raw <- "C:/Users/carlo/King's College London/MT-TNG BioResource ED
 
 filepath_cleaned_data <- "C:/Users/carlo/King's College London/MT-TNG BioResource EDIT - ilovedata - ilovedata/LGBTQ/data/"
 
+filepath_data_raw_coping_nbr <- "C:/Users/carlo/King's College London/MT-TNG BioResource EDIT - ilovedata - ilovedata/LGBTQ/data_raw_coping_nbr"
+
+filepath_cleaned_data_coping_nbr <- "C:/Users/carlo/King's College London/MT-TNG BioResource EDIT - ilovedata - ilovedata/LGBTQ/data_coping_nbr/"
+
 filepath_plots <- "C:/Users/carlo/Documents/work/LGBTQ/plots"
 
 LGBTQ_folder <- "C:/Users/carlo/Documents/work/LGBTQ/"
@@ -24,13 +28,43 @@ filepath_prs_bipolar <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/PRS_bipolar_red
 
 filepath_prs_ptsd <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/PRS_ptsd_reduced.profile"
 
+filepath_prs_adhd <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/PRS_adhd_reduced.profile"
+
+filepath_prs_cud <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/PRS_canabis_reduced.profile"
+
+filepath_prs_smkinit <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/PRS_smkinit_reduced.profile"
+
+#filepath_prs_as <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/PRS_as_reduced.profile"
+
 filepath_MegaPRS_scz <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/MegaPRS_SCZ2022_tab.tsv"
 
 filepath_SBayesRC_scz <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/SCZ_SBayesRC_tab.tsv"
 
 filepath_PRSCS_scz <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/PRS_scz_reduced_PRSCS.profile"
 
+
+
 filepath_prs_pc <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/GLADv3_EDGIv1_NBRv2_EUR_covariates_1kg_projecting_pcs_batch_array.txt"
 
 filepath_prs_same_sex <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/PRS_same_sex_reduced.profile"
+
+
+
+filepath_prs_depression_no_mhd <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/coping_nbr/PRS_depression_reduced.profile"
+
+filepath_prs_an_no_mhd <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/coping_nbr/PRS_an_reduced.profile"
+
+filepath_prs_anxiety_no_mhd <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/coping_nbr/PRS_anxiety_reduced.profile"
+
+filepath_prs_bipolar_no_mhd <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/coping_nbr/PRS_bipolar_reduced.profile"
+
+filepath_prs_ptsd_no_mhd <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/coping_nbr/PRS_ptsd_reduced.profile"
+
+filepath_prs_scz_no_mhd <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/coping_nbr/PRS_scz_reduced.profile"
+
+filepath_prs_adhd_no_mhd <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/coping_nbr/PRS_adhd_reduced.profile"
+
+filepath_prs_cud_no_mhd <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/coping_nbr/PRS_canabis_reduced.profile"
+
+filepath_prs_smkinit_no_mhd <- "C:/Users/carlo/Documents/work/LGBTQ/PRS/coping_nbr/PRS_smkinit_reduced.profile"
 
